02eacf440cfe84a7a1e0b236d1f4e0509084fee7
JordanKPark <50196157+JordanKPark@users.noreply.github.com>
2019-05-05 02:44:20 -0500

    Add files via upload

--- ParkBaseball/Park_Final_Scoreboard/Park_Final_Scoreboard.cpp
+++ ParkBaseball/Park_Final_Scoreboard/Park_Final_Scoreboard.cpp
@@ -555,6 +555,25 @@ void initialize()
 	}
 }
 
+//**************************************************************************
+//this function sets the window size                                       *
+//**************************************************************************
+void SetWindow(int Width, int Height)
+{
+	_COORD coord;
+	coord.X = Width;
+	coord.Y = Height;
+
+	_SMALL_RECT Rect;
+	Rect.Top = 0;
+	Rect.Left = 0;
+	Rect.Bottom = Height - 1;
+	Rect.Right = Width - 1;
+
+	HANDLE Handle = GetStdHandle(STD_OUTPUT_HANDLE);      // Get Handle 
+	SetConsoleScreenBufferSize(Handle, coord);            // Set Buffer Size 
+	SetConsoleWindowInfo(Handle, TRUE, &Rect);            // Set Window Size 
+}
 void gettemp()
 {
 	for (k = 0; k < 9; k++)
@@ -2875,6 +2894,7 @@ void menu()
 
 int main()
 {
+	SetWindow(80, 40);
 	/*************************************
 	*Seed for random number              *
 	**************************************/
